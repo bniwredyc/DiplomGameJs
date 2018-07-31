883791bd0b078925d90d8d866e4a17de60220107
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-07-31 16:47:39 +0300

    исправил ошибку в конструкторе level

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 'use strict';
 
+
 class Vector {
   constructor(x = 0, y = 0) {
     this.x = x
     this.y = y
     }
+
     plus(vector) {
       try {
         if (!(vector instanceof Vector)) {
@@ -13,6 +15,7 @@ class Vector {
 
         let plus_x = this.x + vector.x
         let plus_y = this.y + vector.y
+
         return new Vector(plus_x, plus_y)
 
         } catch(err) {
@@ -20,6 +23,7 @@ class Vector {
         }
 
     }
+
     times(time)  {
       var new_vector = {
         'x' : this.x * time,
@@ -31,7 +35,6 @@ class Vector {
 
 
 class Actor {
-
     constructor(pos = new Vector(), size = new Vector(1 , 1), speed = new Vector()) {
       try {
         if (!((pos instanceof Vector) || (size instanceof Vector) || (speed instanceof Vector)))  {
@@ -81,9 +84,9 @@ class Actor {
           return false
         }
         else if(this.left < someActor.right &&
-           this.right > someActor.scrollLeft &&
-           this.top < someActor.bottom &&
-           this.bottom > someActor.top) {
+          this.right > someActor.scrollLeft &&
+          this.top < someActor.bottom &&
+          this.bottom > someActor.top) {
           return true
         }
         return false
@@ -91,7 +94,7 @@ class Actor {
       } catch (err) {
         console.log(`Ошибка ${err}`)
       }
-    }
+  }
 }
 
 
@@ -102,15 +105,16 @@ class Level  {
       this.height = this.grid.length
 
       if (this.grid.some((element) => Array.isArray(element))) {
-        this.width = is.grid.map((element, i) => i).sort((i, j) =>
-        this.grid[j].length - this.grid[i].length)[0].length
-      } else {
-          this.width = 1
-        }
+        this.width = this.grid.sort(function (i, j) {
+          return j.length - i.length
+        })[0].length
       }else{
-        this.height = 0
-        this.width = 0
+        this.width = 1
       }
+    }else{
+      this.height = 0
+      this.width = 0
+    }
 
     this.status = null
     this.finishDelay = 1
@@ -123,78 +127,77 @@ class Level  {
     }
   }
 
-    isFinished()  {
-      ret this.status != null && finishDelay < 0
+  isFinished()  {
+    return this.status !== null && this.finishDelay < 0
+  }
+
+  actorAt(actor)  {
+    if (!(actor instanceof Actor))  {
+      throw new Error('тип объекта не Actor, или объект не задан')
     }
 
-    actorAt(actor)  {
-      if (!(actor instanceof Actor))  {
-        throw new Error('тип объекта не Actor, или объект не задан')
+    for (let someActor of this.actors)  {
+      if (someActor.isIntersect(actor)) {
+        return someActor
+      }else{
+      return undefined
       }
+    }
+  }
 
-      for (let someActor of this.actors)  {
-        if (someActor.isIntersect(actor)) {
-          return someActor
-        }
-        else{
+  obstacleAt(pos, size)  {
+    if(!(pos instanceof Vector) || !(size instanceof Vector)) {
+      throw new Error('переданные объекты не относяться к типу Vector')
+    }
+
+    const leftBorder = Math.floor(pos.x)
+    const rightBorder = Math.ceil(pos.x + size.x)
+    const topBorder = Math.floor(pos.y)
+    const bottomBorder = Math.ceil(pos.y + size.y)
+
+    if (leftBorder < 0 || rightBorder > this.width || topBorder < 0) {
+      return 'wall'
+    } else if (bottomBorder > this.height) {
+      return 'lava'
+    }
+
+    for (let y = topBorder; y < bottomBorder; y++) {
+      for (let x = leftBorder; x < rightBorder; x++) {
+        if (this.grid[y][x]) {
+          return this.grid[y][x]
+        } else {
           return undefined
         }
       }
     }
+  }
 
-    obstacleAt(pos, size)  {
-      if(!(pos instanceof Vector) || !(size instanceof Vector)) {
-        throw new Error('переданные объекты не относяться к типу Vector')
-      }
+  removeActor(actor)  {
+    let index = this.actors.indexOf(actor)
+    this.actors.splice(index, 1)
+  }
 
-      const leftBorder = Math.floor(position.x)
-      const rightBorder = Math.ceil(position.x + size.x)
-      const topBorder = Math.floor(position.y)
-      const bottomBorder = Math.ceil(position.y + size.y)
-
-      if (leftBorder < 0 || rightBorder > this.width || topBorder < 0) {
-        return 'wall'
-      } else if (bottomBorder > this.height) {
-        return 'lava'
-        }
-
-        for (let y = topBorder; y < bottomBorder; y++) {
-          for (let x = leftBorder; x < rightBorder; x++) {
-            if (this.grid[y][x]) {
-              return this.grid[y][x]
-            }
-            else {
-              return undefined
-            }
-          }
-        }
-      }
-
-    removeActor(actor)  {
-      let index = this.actors.indexOf(actor)
-      this.actors.splice(index, 1)
+  noMoreActors(str_actor) {
+    if (Array.isArray(this.actors)) {
+        return (!this.actors.find(actor => actor.type === str_actor))
     }
+    return true
+  }
 
-    noMoreActors(str_actor) {
-      if (Array.isArray(this.actors)) {
-          return (!this.actors.find(actor => actor.type === str_actor))
-      }
-      return true
-    }
-
-    playerTouched(touchedType, actor)  {
-      if (this.atatus === null) {
-        if (['lava', 'fireball'].some((el) => el === touchedType)) {
-          this.status = 'lost'
-        } else if (touchedType === 'coin' && actor.type === 'coin') {
-          this.removeActor(actor)
+  playerTouched(touchedType, actor)  {
+    if (this.atatus === null) {
+      if (['lava', 'fireball'].some((el) => el === touchedType)) {
+        this.status = 'lost'
+      } else if (touchedType === 'coin' && actor.type === 'coin') {
+        this.removeActor(actor)
         if (this.noMoreActors('coin')) {
-          this.status = 'won'
+        this.status = 'won'
         }
       }
     }
   }
 }
+
 
 class LevelParser {
   constructor(obj) {
@@ -217,7 +220,7 @@ class LevelParser {
 
 class Fireball extends Actor{
   constructor(pos, speed){
-    this.type = fireball
+    this.type = 'fireball'
     this.size = vector
   }
 
